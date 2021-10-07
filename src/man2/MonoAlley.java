e91e6f5ec452afee6d9b1206c639d146dc109de9
NathanL <tzy15368@outlook.com>
2021-10-07 13:10:40 +0200

    add man2 src

---
+++ src/man2/MonoAlley.java
@@ -0,0 +1,25 @@
+//Simple implementation of Alley class
+//Mandatory assignment 2
+//Course 02158 Concurrent Programming, DTU, Fall 2021
+
+//Hans Henrik Lovengreen     Sep 29, 2021
+
+public class MonoAlley extends Alley {
+
+    Semaphore mutex;
+    
+    protected MonoAlley() {
+        mutex = new Semaphore(1);
+    }
+
+    /* Block until car no. may enter alley */
+    public void enter(int no) throws InterruptedException {
+        mutex.P();
+    }
+
+    /* Register that car no. has left the alley */
+    public void leave(int no) {
+        mutex.V();
+    }
+    
+}
