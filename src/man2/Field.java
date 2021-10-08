565e4383da58cc2db31d49f57609e559939f2cf6
NathanL <tzy15368@outlook.com>
2021-10-08 22:05:37 +0200

    solution to problem 2
    *This will cause deadlock

--- src/man2/Field.java
+++ src/man2/Field.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 //Prototype implementation of Field class
 //Mandatory assignment 2
 //Course 02158 Concurrent Programming, DTU, Fall 2021
@@ -6,18 +8,25 @@
 
 public class Field {
 
-    public Field() {
+    private HashMap<Pos,Semaphore> currentPos;
 
+    public Field() {
+        System.out.println("initiated field");
+        currentPos = new HashMap<>();
     }
 
     /* Block until car no. may safely enter tile at pos */
     public void enter(int no, Pos pos) throws InterruptedException {
+        if(!currentPos.containsKey(pos)){
+            currentPos.put(pos, new Semaphore(1));
+        }
+        currentPos.get(pos).P(); 
 
     }
 
     /* Release tile at position pos */
     public void leave(Pos pos) {
-
+        currentPos.get(pos).V();
     }
 
 }
