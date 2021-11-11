3f38a1c74cc08fc424b94080a6e6682bebe01062
NathanL <tzy15368@outlook.com>
2021-11-11 15:28:45 +0100

    add minilab5

---
+++ src/minilab5/donald.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"time"
+)
+
+//var wg sync.WaitGroup
+
+var finished = make(chan string)
+
+func p(name string) {
+	//defer wg.Done()
+
+	for i := 0; i < 3; i++ {
+		time.Sleep(300 * time.Millisecond)
+		fmt.Printf("I am %s\n", name)
+	}
+	//time.Sleep(1 * time.Second)
+	finished <- name
+}
+
+func main() {
+	fmt.Println(runtime.GOMAXPROCS(0))
+	go p("Huey")
+	go p("Dewey")
+	go p("Louie")
+	i := 0
+	// for {
+	// 	var n, ok = <-finished
+	// 	if !ok {
+	// 		fmt.Println("not ok")
+	// 		break
+	// 	}
+	// 	fmt.Println("name" + n)
+	// 	i++
+	// 	if i == 3 {
+	// 		break
+	// 	}
+	// }
+	fmt.Printf("done")
+}
